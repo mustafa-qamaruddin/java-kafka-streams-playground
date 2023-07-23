6e029c01f3f743d356548ab0e1a59f478e763d64
Mustafa Qamar-ud-Din <mustafa.mahrous.89@gmail.com>
2023-07-23 20:34:49 +0200

    Init project + Add producer

---
+++ producer/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "org.qubits"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.kafka:kafka-clients:3.5.1")
+    implementation("ch.qos.logback:logback-core:1.4.8")
+    implementation("ch.qos.logback:logback-classic:1.4.8")
+    compileOnly("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ producer/src/main/java/producer/ProducerApp.java
@@ -0,0 +1,40 @@
+package producer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.RoundRobinPartitioner;
+import org.apache.kafka.common.serialization.DoubleSerializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+
+import java.util.Properties;
+import java.util.Random;
+
+@Slf4j
+public class ProducerApp {
+  public static final String KAFKA_TOPIC = "movies";
+
+  public static void main(String[] args) {
+    Properties properties = new Properties();
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
+    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, DoubleSerializer.class.getName());
+    properties.setProperty(ProducerConfig.PARTITIONER_CLASS_CONFIG, RoundRobinPartitioner.class.getName());
+    KafkaProducer<Integer, Double> kafkaProducer = new KafkaProducer<>(properties);
+    Random random = new Random(101);
+    for (int i = 0; i < 1000; i++) {
+      ProducerRecord<Integer, Double> producerRecord = new ProducerRecord<>(
+        KAFKA_TOPIC,
+        random.nextInt(11),
+        random.nextDouble(5)
+      );
+      kafkaProducer.send(producerRecord,
+        (RecordMetadata metadata, Exception exception) -> log.debug(
+          "offset: {}, timestamp: {}, exception: {}", metadata.offset(), metadata.timestamp(), exception
+        )
+      );
+    }
+  }
+}
