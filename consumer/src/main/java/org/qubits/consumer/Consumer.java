5c334c79443ac52049f6ad1c00caf0044e1aa152
Mustafa Qamar-ud-Din <mustafa.mahrous.89@gmail.com>
2023-08-06 20:18:17 +0200

    Add NUM_THREADS constant to countDownLatch

--- consumer/src/main/java/org/qubits/consumer/Consumer.java
+++ consumer/src/main/java/org/qubits/consumer/Consumer.java
@@ -21,11 +21,26 @@ public class Consumer {
   public static final String TARGET_KAFKA_TOPIC = "movies-enriched";
   public static final String CONSUMER_GROUP = "cg-101";
 
+  public static final int NUM_THREADS = 2;
+
   public static void main(String[] args) {
     Properties properties = new Properties();
     properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "java-kafka-stream-playground-app-v2");
     properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
-    properties.setProperty(StreamsConfig.NUM_STREAM_THREADS_CONFIG, "2");
+    // Each thread can execute one or more tasks with their processor topologies independently.
+    // It is worth noting that there is no shared state amongst the threads, so no inter-thread coordination is necessary.
+    properties.setProperty(StreamsConfig.NUM_STREAM_THREADS_CONFIG, String.format("%d", NUM_THREADS));
+    // If tasks run on a machine that fails and are restarted on another machine, Kafka Streams guarantees to restore
+    // their associated state stores to the content before the failure by replaying the corresponding changelog topics
+    // prior to resuming the processing on the newly started tasks. As a result, failure handling is completely
+    // transparent to the end user.
+    // Note that the cost of task (re)initialization typically depends primarily on the time for restoring the state by
+    // replaying the state stores' associated changelog topics. To minimize this restoration time, users can configure
+    // their applications to have standby replicas of local states (i.e. fully replicated copies of the state). When a
+    // task migration happens, Kafka Streams then attempts to assign a task to an application instance where such a
+    // standby replica already exists in order to minimize the task (re)initialization cost. See num.standby.replicas in
+    // the Kafka Streams Configs section.
+    properties.setProperty(StreamsConfig.NUM_STANDBY_REPLICAS_CONFIG, "3");
 
     // You can specify parameters for the Kafka consumers, producers, and admin client that are used internally.
     properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP);
@@ -43,7 +58,12 @@ public class Consumer {
     // To allow your application to gracefully shutdown in response to SIGTERM
     Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
 
-    final CountDownLatch latch = new CountDownLatch(1);
+    // In addition, Kafka Streams makes sure that the local state stores are robust to failures, too. For each state store,
+    // it maintains a replicated changelog Kafka topic in which it tracks any state updates. These changelog topics are
+    // partitioned as well so that each local state store instance, and hence the task accessing the store, has its own
+    // dedicated changelog topic partition.
+    // TODO cause a failure so that the countDownLatch is tested
+    final CountDownLatch latch = new CountDownLatch(NUM_THREADS);
     kafkaStreams.setStateListener((newState, oldState) -> {
       if (oldState == KafkaStreams.State.RUNNING && newState != KafkaStreams.State.RUNNING) {
         latch.countDown();
